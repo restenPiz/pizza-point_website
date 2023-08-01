c29e3e413c259dd4ed3340b65c2b271576bfbb06
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-01 18:24:31 +0200

    Some updates in website

---
+++ pizza-website/resources/views/Layouts/Principal.blade.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+  
+<!-- Mirrored from yevgenysim-turkey.github.io/incline/restaurant.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 27 Jun 2023 12:20:30 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/ico/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/ico/favicon-16x16.png">
+    <link rel="manifest" href="assets/ico/site.webmanifest">
+    <link rel="mask-icon" href="assets/ico/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="assets/ico/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/libs/flickity/dist/flickity.min.css">
+    <link rel="stylesheet" href="assets/libs/flickity-fade/flickity-fade.css">
+    <link rel="stylesheet" href="assets/libs/fullpage.js/dist/fullpage.min.css">
+    <link rel="stylesheet" href="assets/libs/highlightjs/styles/codepen-embed.css">
+    <link rel="stylesheet" href="assets/libs/%40fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/libs/incline-icons/style.min.css">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="assets/css/theme.min.css">
+
+    <title>Pizza Point - Restaurante e Pizzaria</title>
+  </head>
+  <body>
+
+    <!-- NAVBAR
+    ================================================= -->
+    <nav class="navbar navbar-expand-xl navbar-dark  navbar-togglable  fixed-top">
+      <div class="container">
+
+        <!-- Brand -->
+        <a class="navbar-brand" href="index.html">
+            <img src="./assets/pizza-logo.png" class="" width="100">
+        </a>
+  
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+
+          <!-- Social -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item-divider">
+              <span class="nav-link">
+                <span></span>
+              </span>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="fab fa-facebook"></i> 
+                <span class="d-xl-none ml-2">
+                  facebook
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="fab fa-twitter"></i> 
+                <span class="d-xl-none ml-2">
+                  Twitter
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="fab fa-instagram"></i> 
+                <span class="d-xl-none ml-2">
+                  Instagram
+                </span>
+              </a>
+            </li>
+          </ul>
+
+          <!-- Links -->
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" id="navbarWelcome" >
+                Inicio
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#" id="navbarWelcome" >
+                  Sobre Nos
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#" id="navbarWelcome" >
+                  Menu
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link" href="#" id="navbarWelcome" >
+                  Reserva de Mesa
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link" href="#" id="navbarWelcome" >
+                  Galeria
+                </a>
+              </li>
+            <li class="nav-item">
+              <a href="https://themes.getbootstrap.com/product/incline-landing-page-set/" target="_blank" class="nav-link">
+                Contacto
+              </a>
+            </li>
+          </ul>
+
+        </div> <!-- / .navbar-collapse -->
+  
+      </div> <!-- / .container -->    
+    </nav>
+
+    {{--Inicio da parte contendo o conteudo da website--}}
+
+    @yield('content')
+
+    {{--Fim da parte contendo o conteudo da website--}}
+
+    {{--Inicio do footer--}}
+
+    <footer class="section bg-dark">
+
+        <!-- Triangles -->
+        <div class="bg-triangle bg-triangle-light bg-triangle-bottom bg-triangle-left"></div>
+        <div class="bg-triangle bg-triangle-light bg-triangle-bottom bg-triangle-right"></div>
+  
+        <!--Content -->
+        <div class="container">
+          <div class="row align-self-center">
+            <div class="col-md-auto">
+  
+              <!-- Brand -->
+              <p>
+                <a href="#" class="footer-brand text-white">
+                  <svg class="footer-brand-svg" viewBox="0 0 235 80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <path d="M0 0 L 20 10 L 0 20 Z" class="text-primary" fill="currentColor"></path>
+                    <path d="M0 30 L 20 40 L 0 50 Z M20 45 L 0 55 L 20 65 Z M0 60 L 20 70 L 0 80 Z" fill="currentColor"></path>
+                    <text x="40" y="70" font-family="Arial, sans-serif" font-size="60" font-weight="bold" letter-spacing="-.025em" fill="currentColor">incline.</text>
+                  </svg>
+                </a>
+              </p>
+          
+            </div>
+            <div class="col-md">
+          
+              <!-- Links -->
+              <ul class="list-unstyled list-inline text-md-right">
+                <li class="list-inline-item mr-2">
+                  <a href="#" class="text-white">
+                    Terms and conditions
+                  </a>
+                </li>
+                <li class="list-inline-item mr-2">
+                  <a href="#" class="text-white">
+                    Privacy policy
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" class="text-white">
+                    Contact us
+                  </a>
+                </li>
+              </ul>
+  
+            </div>
+          </div> <!-- / .row -->
+          <div class="row align-items-center">
+            <div class="col-md">
+          
+              <!-- Copyright -->
+              <p class="text-white text-muted">
+                <small>
+                  &copy; Copyright <span class="current-year"></span> Simpleqode. All rights reserved.
+                </small>
+              </p>
+  
+            </div>
+            <div class="col-md">
+          
+              <!-- Social links -->
+              <ul class="list-inline list-unstyled text-md-right">
+                <li class="list-inline-item">
+                  <a href="#">
+                    <i class="fab fa-github"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item ml-3">
+                  <a href="#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item ml-3">
+                  <a href="#">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+  
+            </div>
+          </div> <!-- / .row -->
+        </div> <!-- / .container -->
+  
+      </footer>
+  
+      <!-- JAVASCRIPT
+      ================================================== -->
+      <!-- Global JS -->
+      <script src="assets/libs/jquery/dist/jquery.min.js"></script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCnKt8_N4-FKOnhI_pSaDL7g_g-XI1-R9E"></script>
+  
+      <!-- Plugins JS -->
+      <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/libs/flickity/dist/flickity.pkgd.min.js"></script>
+      <script src="assets/libs/flickity-fade/flickity-fade.js"></script>
+      <script src="assets/libs/jquery-parallax.js/parallax.min.js"></script>
+      <script src="assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+      <script src="assets/libs/waypoints/lib/shortcuts/inview.min.js"></script>
+      <script src="assets/libs/fullpage.js/vendors/scrolloverflow.min.js"></script>
+      <script src="assets/libs/fullpage.js/dist/fullpage.min.js"></script>
+      <script src="assets/libs/highlightjs/highlight.pack.min.js"></script>
+  
+      <!-- Theme JS -->
+      <script src="assets/js/theme.min.js"></script>
+  
+    </body>
+  
+  <!-- Mirrored from yevgenysim-turkey.github.io/incline/restaurant.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 27 Jun 2023 12:21:05 GMT -->
+  </html>
